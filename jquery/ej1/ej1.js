5b2a98d895ee0c57f95d78002b6e1e5a7ccd5fc2
Andrea Cordón Barrionuevo <92718546+andrea25cb@users.noreply.github.com>
2022-11-06 22:40:25 +0100

    Add files via upload

---
+++ jquery/ej1/ej1.js
@@ -0,0 +1,9 @@
+//$('#parrafos').find('p').eq(0).html('JSHAKJD');
+
+$(function() {
+    $("#boton").on("click", function() {
+        $('*').hide(); //oculto mediante id
+        $('*').hide(); //muestro mediante clase
+    });
+
+});
